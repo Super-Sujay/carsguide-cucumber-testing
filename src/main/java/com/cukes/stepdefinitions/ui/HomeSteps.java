44f02048d53a2d1d0902af85e9be7246e2b42007
Sujay Sawant <sujayvsawant@gmail.com>
2020-11-18 18:29:01 +0530

    Added the latest version of dependencies.

--- src/main/java/com/cukes/stepdefinitions/ui/HomeSteps.java
+++ src/main/java/com/cukes/stepdefinitions/ui/HomeSteps.java
@@ -3,13 +3,12 @@ package com.cukes.stepdefinitions.ui;
 import com.cukes.pages.HomePage;
 
 import io.cucumber.java8.En;
-import io.cucumber.java8.StepDefinitionBody.A0;
 
 public class HomeSteps implements En {
 
 	public HomeSteps(HomePage homePage) {
 		Given("I am on the home page {string} of cars guide website", homePage::openPage);
-		When("I move to Cars For Sale menu", (A0) homePage.buyAndSell::moveMousePointer);
+		When("I move to Cars For Sale menu", homePage.buyAndSell::moveMousePointer);
 		Then("I click on {string}", homePage::clickElementWithLinkText);
 	}
 
